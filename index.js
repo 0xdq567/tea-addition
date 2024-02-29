d14465b29cd846174260bd7abdd536e0c0101826
598471284 <598471284@qq.com>
2024-02-29 13:45:47 +0800

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+/**
+ * 将两个数字相加
+ * @param {number} a 第一个数字
+ * @param {number} b 第二个数字
+ * @returns {number} 两个数字的和
+ */
+function add(a, b) {
+    return a + b;
+  }
+  
+  // 导出 add 函数，使其可以在其他地方使用
+  module.exports = add;
+  
